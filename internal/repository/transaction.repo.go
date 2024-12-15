644d9fd22b150392370a06b98b012edec8a6050e
Damshxy <mrsaddam2806@gmail.com>
2024-12-15 14:27:14 +0700

    feat(refund transaction): refund transaction (#1)

--- internal/repository/transaction.repo.go
+++ internal/repository/transaction.repo.go
@@ -8,8 +8,8 @@ import (
 type TransactionRepository interface {
 	CreateTransaction(transaction *models.Transaction) error
 	GetTransactionByConsumerID(consumerID int) ([]*models.Transaction, error)
-	UpdateTransaction(transaction *models.Transaction) error
-	DeleteTransaction(id int) error
+	GetTransactionByID(id int) (*models.Transaction, error)
+	MarkTransactionAsRefund(transactionID int) error
 }
 
 type transactionRepository struct {
@@ -31,6 +31,16 @@ func (r *transactionRepository) CreateTransaction(transaction *models.Transactio
 	return nil
 }
 
+func (r *transactionRepository) GetTransactionByID(id int) (*models.Transaction, error) {
+	var transaction models.Transaction
+	err := r.db.First(&transaction, id).Error
+	if err != nil {
+		return &transaction, err
+	}
+
+	return &transaction, nil
+}
+
 func (r *transactionRepository) GetTransactionByConsumerID(consumerID int) ([]*models.Transaction, error) {
 	var transactions []*models.Transaction
 	err := r.db.Where("consumer_id = ?", consumerID).Find(&transactions).Error
@@ -41,17 +51,8 @@ func (r *transactionRepository) GetTransactionByConsumerID(consumerID int) ([]*m
 	return transactions, nil
 }
 
-func (r *transactionRepository) UpdateTransaction(transaction *models.Transaction) error {
-	err := r.db.Save(transaction).Error
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (r *transactionRepository) DeleteTransaction(id int) error {
-	err := r.db.Delete(&models.Transaction{}, id).Error
+func (r *transactionRepository) MarkTransactionAsRefund(transactionID int) error {
+	err := r.db.Model(&models.Transaction{}).Where("id = ?", transactionID).Update("refunded", true).Error
 	if err != nil {
 		return err
 	}
